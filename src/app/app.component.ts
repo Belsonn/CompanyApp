54f327da0c8a18193cb7e41adba88551237bba5e
Mateusz Belka <mateusz.belka14@gmail.com>
2020-03-16 08:22:11 +0100

    testing fetching data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CompaniesService } from "./shared/companies.service";
+import { Companies } from "./shared/companies.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'companiesApp';
+export class AppComponent implements OnInit {
+  companies: Companies[] = [];
+  constructor(private companiesService: CompaniesService) {}
+
+  ngOnInit() {
+    this.companiesService.fetchCompanies().subscribe(companies => {
+      this.companiesService.lenght = companies.length;
+      this.companiesService.fetchIncome(companies);
+    });
+  }
+
+  onGet() {
+    this.companiesService.calcIncome();
+  }
+  onIncome() {
+    if (this.companiesService.lenght < 1) {
+      console.log(this.companiesService.companies);
+    }
+  }
 }
--- src/app/shared/companies.service.ts
+++ src/app/shared/companies.service.ts
@@ -7,6 +7,7 @@ import { Income } from "./income.model";
 @Injectable({ providedIn: "root" })
 export class CompaniesService {
   companies: Companies[] = [];
+  lenght: number;
   isFetching: boolean = true;
 
   constructor(private http: HttpClient) {}
@@ -24,24 +25,18 @@ export class CompaniesService {
         .get<{ id: number; incomes: Income[] }>(
           `https://recruitment.hal.skygate.io/incomes/${companies[index].id}`
         )
-        .pipe(
-          map(compIncomes => {
-            console.log(compIncomes);
-            companie.incomes = compIncomes.incomes;
-            return companie;
-          })
-        )
-        .subscribe(incCompanies => {
-          this.companies.push(incCompanies);
+        .subscribe(incom => {
+          companie.incomes = incom.incomes;
+          this.companies.push(companie);
+          this.lenght--;
         });
     }
   }
   calcIncome() {
     for (const company of this.companies) {
-        company.totalIncome = 0;
+      company.totalIncome = 0;
       for (const singleIncome of company.incomes) {
         let inc: number = +singleIncome.value;
-        console.log(inc, company.totalIncome);
         company.totalIncome += inc;
       }
     }
