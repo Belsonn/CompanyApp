28457dc8934ebcf3893c8da57f3800840d3560ef
Mateusz Belka <mateusz.belka14@gmail.com>
2020-03-17 04:04:07 +0100

    initial of detailpage

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Companies } from "../shared/companies.model";
+import { CompaniesService } from "../shared/companies.service";
+import { Income } from '../shared/income.model';
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.css"]
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  company: Companies;
+  averageIncome: number = 0;
+  lastMonthIncome: number = 0;
+  constructor(
+    private route: ActivatedRoute,
+    private companiesService: CompaniesService
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params["id"];
+    this.company = this.companiesService.companies.find(el => id == el.id);
+    this.calcAverageIncome(this.company);
+    this.calcLastMonthIncome(this.company);
   }
 
+  calcAverageIncome(company: Companies) {
+    this.averageIncome = company.totalIncome / company.incomes.length;
+  }
+  calcLastMonthIncome(company: Companies) {
+    let date = new Date();
+    if(date.getMonth() == 0){
+      date.setMonth(11)
+      date.setFullYear(date.getFullYear() - 1); 
+    }
+    else {
+      date.setMonth(date.getMonth() - 1);
+    }
+    for(const index of company.incomes){
+      if(index.date.getMonth() == date.getMonth() && index.date.getFullYear() == date.getFullYear()) {
+        this.lastMonthIncome += +index.value;
+      }
+    }
+  }
 }
