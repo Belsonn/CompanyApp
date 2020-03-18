b06d163276f37dc6f18e153d1e9185bb80f2c027
Mateusz Belka <mateusz.belka14@gmail.com>
2020-03-18 17:08:59 +0100

    subscribe error handler

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { CompaniesService } from '../shared/companies.service';
+import { CompaniesService } from "../shared/companies.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-home",
@@ -9,14 +10,14 @@ import { CompaniesService } from '../shared/companies.service';
 export class HomeComponent implements OnInit {
   constructor(private companiesService: CompaniesService) {}
   length: number;
-  error: Error
+  error = null;
 
   // it has length property for check if data is already here and use ngIf properly
   // it calculates Total Income because its needed for this view
   // subscring all companies and for each one subscribing to get incomes for this single company
 
   ngOnInit() {
-    if(!(this.companiesService.companies.length > 0)){
+    if (!(this.companiesService.companies.length > 0)) {
       this.companiesService.fetchCompanies().subscribe(companiesAPI => {
         this.length = companiesAPI.length;
         for (const index of companiesAPI) {
@@ -24,11 +25,22 @@ export class HomeComponent implements OnInit {
             companieOne.calcTotalIncome();
             this.companiesService.companies.push(companieOne);
             this.length--;
+          }, error => {
+            this.error = error.message;
+            this.length = 0;
           });
         }
+      }, error => {
+        this.length = 0;
+        this.error = error.message;
       });
     } else {
       this.length = 0;
     }
+  }
+
+  onHandleError() {
+    this.error = null;
+    window.location.reload();
   }
 }
